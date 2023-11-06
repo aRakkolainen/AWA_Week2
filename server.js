c610dd791a8c1c3317fa86c890af01a387565a34
aRakkolainen <aino.rakkolainen@gmail.com>
2023-11-06 11:32:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,3 +10,13 @@ app.listen(port, () => console.log("Server is listening to a port: "+port));
 app.get('/echo/:id', (req, res) => {
     res.send({"id": req.params.id})
 })
+
+app.post('/sum', (req, res) => {
+    let sum =0;
+    let numbers = req.numbers;
+    numbers.array.forEach(number => {
+        sum += numbers;
+    });
+    res.send({"sum": sum});
+
+    })
