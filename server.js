d0ff74954cc7f98dfaa37ea5073fe5c2a1458d54
aRakkolainen <aino.rakkolainen@gmail.com>
2023-11-06 10:33:53 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const port = 3000; 
+app.get('/hello', (req, res) => {
+    res.send("Hello world");
+})
+
+app.listen(port, () => console.log("Server is listening to a port: "+port));
