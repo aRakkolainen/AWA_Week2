2a02265aaff196060fb3eb1d41333ac640ef9957
aRakkolainen <aino.rakkolainen@gmail.com>
2023-11-06 11:03:49 +0200

    Init

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000; 
 app.get('/hello', (req, res) => {
-    res.send("Hello world");
+    res.send({"msg":"Hello world"});
 })
 
 app.listen(port, () => console.log("Server is listening to a port: "+port));
