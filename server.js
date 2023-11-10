041e1815cd9eabfa1198486952bc66b273d311ca
aRakkolainen <aino.rakkolainen@gmail.com>
2023-11-10 12:39:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,9 +20,14 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", routes);*/ 
 
 app.post("/list", (req, res) => {
-    list.push(req.body.text)
-    console.log(list)
-    res.send(list)
+    if (req.body.text != "undefined") {
+        list.push(req.body.text)
+        res.send(list)
+    }
+    //console.log(req.body.text)
+    //list.push(req.body.text)
+    //console.log(list)
+    //res.send(list)
 })
 
 
@@ -31,37 +36,3 @@ app.listen(port, function(err) {
     if (err) console.log(err);
     console.log("Server is listening to a port", port);
 });
-//app.use(express.json());
-/*app.post('/sum', function (req, res) {
-    let sum = 0;
-    console.log(req)
-    //let numbers = req.Route.numbers
-    //console.log("Receiving post request")
-    //console.log(numbers)
-    //console.log(req.numbers)
-    //console.log(req.body.route.numbers)
-    //console.log(numbers)
-    //numbers.array.forEach(number => {
-      //sum += number;
-      //console.log(sum)
-    //});
-    res.send({"sum": sum});
-    })
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-
-})
-let list = []; 
-
-app.post("/list", (req, res) => {
-    list.push(req.body)
-    res.send(list)
-})
-//app.listen(port, () => console.log("Server is listening to a port: "+port));
-app.listen(port, function(err) {
-    if (err) console.log(err);
-    console.log("Server is listening to a port", port);
-});
-
-module.exports = router*/
