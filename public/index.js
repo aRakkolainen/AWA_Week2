181a1a04a18d82826813c3df9ed8e24838848202
aRakkolainen <aino.rakkolainen@gmail.com>
2023-11-10 12:31:42 +0200

    Task5

--- public/index.js
+++ public/index.js
@@ -3,13 +3,15 @@ const inputText = document.getElementById("input-text");
 button.addEventListener("click", () => {
     //let input = {"text": inputText.value}
     // Sending POST request: https://www.youtube.com/watch?v=xJAxjstgITk 
-    fetch("http://localhost:3000/list", 
-    {method: 'POST',
-    headers: {
+    if (inputText.value != "") {
+        fetch("http://localhost:3000/list", 
+        {method: 'POST',
+        headers: {
         "Content-type": "application/json"
-    }, 
-    body: '{"text": "' + inputText.value + '"}'
-    })
-    .then(results => results.json())
-    .then(console.log);
+        }, 
+        body: '{"text": "' + inputText.value + '"}'
+        })
+        .then(results => console.log(results))
+    }
+    
 })
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const router = express.Router(); 
 const port = 3000; 
+let list = [];
 app.get('/hello', (req, res) => {
     res.send({"msg":"Hello world"});
 })
@@ -11,17 +11,41 @@ app.get('/echo/:id', (req, res) => {
     res.send({"id": req.params.id})
 })
 app.use(express.json());
-app.post('/sum', function (req, res) {
-    let sum =0;
-    //let numbers = req.body
-    console.log("Receiving post request")
-    console.log(req.route)
+app.post("/sum", (req, res) => {
+    
+})
+
+app.use(express.static(path.join(__dirname, "public")));
+/*const routes = require("./routes.js"); 
+app.use("/", routes);*/ 
+
+app.post("/list", (req, res) => {
+    list.push(req.body.text)
+    console.log(list)
+    res.send(list)
+})
+
+
+
+app.listen(port, function(err) {
+    if (err) console.log(err);
+    console.log("Server is listening to a port", port);
+});
+//app.use(express.json());
+/*app.post('/sum', function (req, res) {
+    let sum = 0;
+    console.log(req)
+    //let numbers = req.Route.numbers
+    //console.log("Receiving post request")
+    //console.log(numbers)
+    //console.log(req.numbers)
     //console.log(req.body.route.numbers)
     //console.log(numbers)
     //numbers.array.forEach(number => {
-      //  sum += number;
+      //sum += number;
+      //console.log(sum)
     //});
-    //res.send({"sum": sum});
+    res.send({"sum": sum});
     })
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -40,4 +64,4 @@ app.listen(port, function(err) {
     console.log("Server is listening to a port", port);
 });
 
-module.exports = router
+module.exports = router*/
